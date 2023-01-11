2f09ce3ba98ad324aafebd62c5761e3d80c56610
xanrass <122484284+xanrass@users.noreply.github.com>
2023-01-11 22:56:48 +0300

    Create mention.js

---
+++ commands/mention.js
@@ -0,0 +1,9 @@
+module.exports =[{
+  name: "<@$clientID>",
+  nonPrefixed: true,
+  code: `Merhaba, bu sunucuda **$getServerVar[prefix]** sembolü prefiximdir.`
+}, {
+  name: "<@!$clientID>",
+  nonPrefixed: true,
+  code: `Merhaba, bu sunucuda **$getServerVar[prefix]** sembolü prefiximdir.`
+}] 
